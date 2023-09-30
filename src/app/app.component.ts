07a9832a4663bbcbb68947b734a2d00bfeddb9f8
thomiboi <thomasoliver.r@outlook.com>
2023-09-30 19:21:22 -0300

    cambiado inicio, se agrego un swiper.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   constructor() {}
 }
+
+register();
