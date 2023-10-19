2a0a406295f0fc6c8c01dfdef64c3f81dfbdade8
thomiboi <thomasoliver.r@outlook.com>
2023-10-19 16:59:23 -0300

    Agregado firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 //Se agrega plugin de awesome cordova para ser reconocido
 import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
-
+//se agrega el plugin de angular fire
+import { AngularFireModule } from '@angular/fire/compat';
+//Se agrega el plugin de angular para la auth
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
 
 //NOTA: se agrega 'SQLite' a providers
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)], 
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },SQLite],
   bootstrap: [AppComponent],
 })
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,18 +1,40 @@
-import { Component, ViewChildren, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, ViewChild, Inject } from '@angular/core';
 import { IonCard, AnimationController } from '@ionic/angular';
 import type {QueryList} from '@angular/core';
 import type {Animation} from '@ionic/angular';
+import type {LoadingController} from '@ionic/angular';
+//se implementa los validadores y el form de angular
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage {
+export class RegistroPage implements OnInit{
+  //se declara el regForm 
+  regForm: FormGroup | undefined;
 
   
+  //se agrega el formbuilder, se agrego el inject, aunque no se muy bien para que aun.
+  constructor(public formBuilder: FormBuilder, @Inject(String) LoadingCtrl: LoadingController, private animationController: AnimationController) {
+  }
 
-  constructor(private animationController: AnimationController) {}
+  ngOnInit(){
+    this.regForm = this.formBuilder.group({
+      nombre:['',[Validators.required]],
+      apellido:['',[Validators.required]],
+      fechaNac:['',[Validators.required]],
+      email:['',[Validators.required, Validators.email, 
+        Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$")]],
+      password:['',[Validators.required, Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")]]
+      
+    })
+  }
+
+  get errorControl(){
+    return this.regForm?.controls;
+  }
 
   ngAfterViewInit() {
     const animar1Animation = this.animationController
