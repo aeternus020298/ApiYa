f199cb182b1d8fd0e337a21d8a3a8f260c22cd39
thomiboi <thomasoliver.r@outlook.com>
2023-10-19 22:27:19 -0300

    funciono ctm!!!!

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,43 +1,66 @@
-import { Component, OnInit, ViewChildren, ElementRef, ViewChild, Inject } from '@angular/core';
-import { IonCard, AnimationController } from '@ionic/angular';
-import type {QueryList} from '@angular/core';
-import type {Animation} from '@ionic/angular';
-import type {LoadingController} from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { IonCard, AnimationController } from "@ionic/angular";
+import type { QueryList } from "@angular/core";
+import type { Animation } from "@ionic/angular";
+import { LoadingController } from "@ionic/angular";
+import { FirebaseAuthService } from "src/app/services/firebase-auth.service";
 //se implementa los validadores y el form de angular
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.page.html',
-  styleUrls: ['./registro.page.scss'],
+  selector: "app-registro",
+  templateUrl: "./registro.page.html",
+  styleUrls: ["./registro.page.scss"],
 })
-export class RegistroPage implements OnInit{
-  //se declara el regForm 
-  regForm: FormGroup | undefined;
+export class RegistroPage implements OnInit {
+  //se declara el regForm
+  regForm: FormGroup;
 
-  
   //se agrega el formbuilder, se agrego el inject, aunque no se muy bien para que aun.
-  constructor(public formBuilder: FormBuilder, @Inject(String) LoadingCtrl: LoadingController, private animationController: AnimationController) {
-  }
+  constructor(
+    public formBuilder: FormBuilder,
+    public loadingCtrl: LoadingController,
+    public authService: FirebaseAuthService,
+    private animationController: AnimationController
+  ) {}
 
-  ngOnInit(){
+  //declaro todas las validaciones del forms, asi como dejo en forma obligatoria esos parametros del registro
+  ngOnInit() {
     this.regForm = this.formBuilder.group({
-      nombre:['',[Validators.required]],
-      apellido:['',[Validators.required]],
-      fechaNac:['',[Validators.required]],
-      email:['',[Validators.required, Validators.email, 
-        Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$")]],
-      password:['',[Validators.required, Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")]]
-      
-    })
+      nombre: ["", [Validators.required]],
+      apellido: ["", [Validators.required]],
+      fechaNac: ["", [Validators.required]],
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$"),
+        ],
+      ],
+      password: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}"),
+        ],
+      ],
+    });
   }
 
-  get errorControl(){
+  get errorControl() {
     return this.regForm?.controls;
+  }
+  //agrego un metodo que me permite crear una pantalla de carga mientras se registra el usuario en firebase.
+  async signUp() {
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    if (this.regForm?.valid) {
+      //const user = await this.authService.registerUser(email,password)
+    }
   }
 
   ngAfterViewInit() {
-    const animar1Animation = this.animationController
-      
+    const animar1Animation = this.animationController;
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,26 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'lagna-login-registro',
-    appId: '1:395943183455:web:2bceab02900119058e47ea',
-    storageBucket: 'lagna-login-registro.appspot.com',
-    apiKey: 'AIzaSyC3LbeZkDIcbkEVJRMI3VJIR1-DLC6E7mQ',
-    authDomain: 'lagna-login-registro.firebaseapp.com',
-    messagingSenderId: '395943183455',
-    measurementId: 'G-LDP5Z088B4',
-  },
   production: false,
   //Se agrega la configuracion SDK del firebase para Lagna.
-  firebaseConfig : {
+  firebaseConfig: {
     apiKey: "AIzaSyC3LbeZkDIcbkEVJRMI3VJIR1-DLC6E7mQ",
     authDomain: "lagna-login-registro.firebaseapp.com",
     projectId: "lagna-login-registro",
     storageBucket: "lagna-login-registro.appspot.com",
     messagingSenderId: "395943183455",
     appId: "1:395943183455:web:2bceab02900119058e47ea",
-    measurementId: "G-LDP5Z088B4"
-  }
+    measurementId: "G-LDP5Z088B4",
+  },
 };
 
 /*
