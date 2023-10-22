1ec5b9c2522196a445ae7f319ad41fec075b5e8f
thomiboi <thomasoliver.r@outlook.com>
2023-10-22 02:35:57 -0300

    arreglado error en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  firebase: {
-    projectId: 'lagna-login-registro',
-    appId: '1:395943183455:web:2bceab02900119058e47ea',
-    storageBucket: 'lagna-login-registro.appspot.com',
-    apiKey: 'AIzaSyC3LbeZkDIcbkEVJRMI3VJIR1-DLC6E7mQ',
-    authDomain: 'lagna-login-registro.firebaseapp.com',
-    messagingSenderId: '395943183455',
-    measurementId: 'G-LDP5Z088B4',
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3LbeZkDIcbkEVJRMI3VJIR1-DLC6E7mQ",
+    authDomain: "lagna-login-registro.firebaseapp.com",
+    projectId: "lagna-login-registro",
+    storageBucket: "lagna-login-registro.appspot.com",
+    messagingSenderId: "395943183455",
+    appId: "1:395943183455:web:2bceab02900119058e47ea",
+    measurementId: "G-LDP5Z088B4",
   },
-  production: true
 };
