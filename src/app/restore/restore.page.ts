bb2b7b03307a2bab717b7abb175117696e10fedd
thomiboi <thomasoliver.r@outlook.com>
2023-10-30 10:56:32 -0300

    avance en el restore

--- src/app/restore/restore.page.ts
+++ src/app/restore/restore.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FirebaseAuthService } from "../services/firebase-auth.service";
 
 @Component({
-  selector: 'app-restore',
-  templateUrl: './restore.page.html',
-  styleUrls: ['./restore.page.scss'],
+  selector: "app-restore",
+  templateUrl: "./restore.page.html",
+  styleUrls: ["./restore.page.scss"],
 })
 export class RestorePage implements OnInit {
+  email: any;
 
-  constructor() { }
+  constructor(public authService: FirebaseAuthService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async resetPassword() {
+    this.authService.resetPassword(this.email);
   }
-
 }
