6b50c78c2c9fa5c39c0347824189d2e16558b829
thomiboi <thomasoliver.r@outlook.com>
2023-10-31 12:52:27 -0300

    restore editado

--- src/app/restore/restore.page.ts
+++ src/app/restore/restore.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FirebaseAuthService } from "../services/firebase-auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-restore",
@@ -9,11 +10,21 @@ import { FirebaseAuthService } from "../services/firebase-auth.service";
 export class RestorePage implements OnInit {
   email: any;
 
-  constructor(public authService: FirebaseAuthService) {}
+  constructor(public router: Router, public authService: FirebaseAuthService) {}
 
   ngOnInit() {}
 
   async resetPassword() {
-    this.authService.resetPassword(this.email);
+    this.authService
+      .resetPassword(this.email)
+      .then(() => {
+        console.log("link enviado");
+        this.router.navigate(["/login"]).then(() => {
+          this.email = ""; // Restablecer el valor del email a una cadena vacía
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
