3271447107ae884bb572a296e9201a766fc1a4de
thomiboi <thomasoliver.r@outlook.com>
2023-11-01 22:40:47 -0300

    animacion en restore

--- src/app/restore/restore.module.ts
+++ src/app/restore/restore.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { IonicModule } from "@ionic/angular";
+import { RestorePageRoutingModule } from "./restore-routing.module";
+import { RestorePage } from "./restore.page";
+import { defineCustomElements } from "@teamhive/lottie-player/loader";
+import { MatSliderModule } from "@angular/material/slider";
 
-import { IonicModule } from '@ionic/angular';
-
-import { RestorePageRoutingModule } from './restore-routing.module';
-
-import { RestorePage } from './restore.page';
-
+defineCustomElements(window);
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    MatSliderModule,
     IonicModule,
-    RestorePageRoutingModule
+    RestorePageRoutingModule,
   ],
-  declarations: [RestorePage]
+  declarations: [RestorePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class RestorePageModule {}
