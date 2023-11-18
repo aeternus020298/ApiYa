c99cc01b87a11aaa1c6bf5f7441487e6f25e739c
MiguelThomasGrupo10 <mi.leivac@duocuc.cl>
2023-11-18 19:00:16 -0300

    apicocteles creada y funcionando

--- src/app/services/apicocteles.service.ts
+++ src/app/services/apicocteles.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { retry, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,9 @@ export class ApicoctelesService {
     })
   }
 
-  //Se establece la base url del API a consumir
+  //Se establece la base url del API a consumir, BodasAlrededor
   apiurl = 'https://apibodas-fb7d7-default-rtdb.firebaseio.com/apibodas/-NhSSwjag7FA_yVXP7jg/BodasAlrededor.json';
+  apicocktail = 'https://apicocteles-default-rtdb.firebaseio.com/Cocktail.json';
 
   //Se declara la variable http de tipo HttpClient
   constructor(private http:HttpClient) { }
@@ -29,5 +31,11 @@ export class ApicoctelesService {
     );
   }
   
+  getPostsCocktail(): Observable<any> {
+    return this.http.get(this.apicocktail).pipe(
+      retry(3)
+    );
+  }
+
   
 }
