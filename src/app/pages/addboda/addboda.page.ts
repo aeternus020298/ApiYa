52c17b24121f1ec4c77aef7187648575f84a107a
MiguelThomasGrupo10 <mi.leivac@duocuc.cl>
2023-11-19 20:28:58 -0300

    ion select en addboda

--- src/app/pages/addboda/addboda.page.ts
+++ src/app/pages/addboda/addboda.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { DbserviceService } from "../../services/dbservice.service";
+import { ApicoctelesService } from "src/app/services/apicocteles.service";
+
 
 @Component({
   selector: "app-addboda",
@@ -15,7 +17,12 @@ export class AddbodaPage implements OnInit {
   lugarBoda = "";
   fechaBoda = "";
 
-  constructor(private dbservice: DbserviceService, private router: Router) {}
+  apicocteles: any = [];
+
+  constructor(private dbservice: DbserviceService, 
+    private apiService: ApicoctelesService, 
+    private router: Router, 
+    ) {}
 
   guardar() {
     this.dbservice.addBoda(
@@ -30,5 +37,15 @@ export class AddbodaPage implements OnInit {
     this.router.navigate(["/inicio"]);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.cargarCocktails();
+  }
+  
+  cargarCocktails() {
+    this.apiService.getPostsCocktail().subscribe(data => {
+      this.apicocteles = data;
+    });
+  }
+
+  
 }
